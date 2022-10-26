7e2c3f58b2dc92b7ee5a72b34ed1da373ff43bd2
kalinvladwork <kalinvlad.work@gmail.com>
2022-10-27 00:55:27 +0300

    create

---
+++ script/main.js
@@ -0,0 +1,51 @@
+let modal = document.querySelector('#modalContainer'),
+    body = document.querySelector('body'),
+    choice = document.querySelector('#choice'),
+    homeownerTrue = document.querySelector('#homeowner_true'),
+    homeownerFalse = document.querySelector('#homeowner_false'),
+    activeForm = document.querySelector('.formSection.formSection_active'),
+    stepsProgress = document.querySelector('.stepsProgress__step'),
+    nextButton = document.querySelector('.button[data-next]');
+
+function scrolling() {
+    event.preventDefault()
+    document.getElementById('sectionForm').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function openModal() {
+    event.preventDefault()
+    modal.removeAttribute('class');
+    modal.classList.add('is-open');
+    body.classList.add('modal-active');
+}
+
+function closeModal() {
+    event.preventDefault()
+    modal.classList.add('out');
+    body.classList.remove('modal-active');
+}
+
+function nextStep() {
+    if (!homeownerTrue.checked && !homeownerFalse.checked) {
+        nextButton.getAttribute('disabled')
+        return false
+    }
+
+    if (homeownerTrue.checked) {
+        choice.innerText = 'Yes';
+    } else if (homeownerFalse.checked) {
+        choice.innerText = 'No';
+    }
+
+    activeForm.classList.remove('formSection_active');
+    activeForm.nextElementSibling.classList.add('formSection_active');
+    stepsProgress.classList.add('stepsProgress__step_full')
+}
+
+function submitForm() {
+    event.preventDefault();
+    closeModal();
+}
+
